2f5f22fbcbbe6b486baf7ceee378a5b1c3acf3bb
Avery Walker <spikypancakedragon@gmail.com>
2024-02-26 20:14:54 -0600

    made normal an object with it's own tick method, but it doesn't work for some reason

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,10 +8,8 @@ class GildedRose
   end
 
   def normal_tick
-    @days_remaining -=1
-    return if quality == 0
-    @quality -= 1
-    @quality -= 1 if @days_remaining <=0
+    @item = Normal.new(quality, days_remaining)
+    @item.tick
   end
 
   def brie_tick
@@ -47,6 +45,24 @@ class GildedRose
     when 'Backstage passes to a TAFKAL80ETC concert'
       return backstage_tick
     end
+  end
+
+end
+
+
+
+class Normal
+  attr_reader :quality, :days_remaining
+
+  def initialize(quality, days_remaining)
+    @quality, @days_remaining = quality, days_remaining
+  end
+
+  def tick
+    @days_remaining -= 1
+    return if quality == 0
+    @quality -= 1
+    @quality -= 1 if @days_remaining <= 0
   end
 
 end
