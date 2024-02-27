19d65ae94e2133e4cad1a2440246626cad8293c5
Avery Walker <spikypancakedragon@gmail.com>
2024-02-26 19:22:59 -0600

    found bug in brie_tick method, fixed it

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -17,10 +17,10 @@ class GildedRose
   def brie_tick
     @days_remaining -= 1
     return if @quality >= 50
-    @quality += 1
-    @quality += 1 if @days_remaining <= 0
-  end
 
+    @quality += 1
+    @quality += 1 if @days_remaining <= 0 && @quality < 50
+  end
 
 
   def tick
