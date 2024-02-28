ce797bdfdf23cf5641e7345273c3a5c95048d6ea
Avery Walker <spikypancakedragon@gmail.com>
2024-02-27 19:51:57 -0600

    made GildedRose into a module, got rid of other unnecessary stuff

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,11 +1,10 @@
-class GildedRose
-  attr_reader :name, :days_remaining, :quality
+module GildedRose
 
-  def initialize(name:, days_remaining:, quality:)
-    @item = klass_for(name).new(quality, days_remaining)
+  def self.new(name:, days_remaining:, quality:)
+    klass_for(name).new(quality, days_remaining)
   end
 
-  def klass_for(name)
+  def self.klass_for(name)
     case name
     when 'Normal Item'
       Normal
@@ -16,18 +15,6 @@ class GildedRose
     when 'Backstage passes to a TAFKAL80ETC concert'
       Backstage
     end
-  end
-
-  def quality
-    @item.quality
-  end
-
-  def days_remaining
-    @item.days_remaining
-  end
-
-  def tick
-    @item.tick
   end
 
 end
