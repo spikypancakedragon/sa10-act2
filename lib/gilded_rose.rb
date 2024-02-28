1427f40883c792b6ea555b715817e2ddaa3030d2
Avery Walker <spikypancakedragon@gmail.com>
2024-02-27 18:52:51 -0600

    added sulfuras and backstage classes

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -28,17 +28,13 @@ class GildedRose
   end
 
   def sulfuras_tick
-
+    @item = Sulfuras.new(@quality, @days_remaining)
+    @item.tick
   end
 
   def backstage_tick
-    @days_remaining -= 1
-    return if @quality >= 50
-    return @quality = 0 if @days_remaining < 0
-
-    @quality += 1
-    @quality += 1 if @days_remaining < 10
-    @quality += 1 if @days_remaining < 5
+    @item = Backstage.new(@quality, @days_remaining)
+    @item.tick
   end
 
   def tick
@@ -87,6 +83,37 @@ class Brie
 
     @quality += 1
     @quality += 1 if @days_remaining <= 0 && @quality < 50
+  end
+
+end
+
+class Sulfuras
+  attr_reader :quality, :days_remaining
+
+  def initialize(quality, days_remaining)
+    @quality, @days_remaining = quality, days_remaining
+  end
+
+  def tick
+  end
+
+end
+
+class Backstage
+  attr_reader :quality, :days_remaining
+
+  def initialize(quality, days_remaining)
+    @quality, @days_remaining = quality, days_remaining
+  end
+
+  def tick
+    @days_remaining -= 1
+    return if @quality >= 50
+    return @quality = 0 if @days_remaining < 0
+
+    @quality += 1
+    @quality += 1 if @days_remaining < 10
+    @quality += 1 if @days_remaining < 5
   end
 
 end
