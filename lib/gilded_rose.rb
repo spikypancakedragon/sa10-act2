b8d3df8abe589bfc50128373f3f522c019c4d708
Avery Walker <spikypancakedragon@gmail.com>
2024-02-27 19:47:14 -0600

    moved case statment to klass_for method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -2,36 +2,32 @@ class GildedRose
   attr_reader :name, :days_remaining, :quality
 
   def initialize(name:, days_remaining:, quality:)
-    @name = name
-    @days_remaining = days_remaining
-    @quality = quality
+    @item = klass_for(name).new(quality, days_remaining)
+  end
+
+  def klass_for(name)
+    case name
+    when 'Normal Item'
+      Normal
+    when 'Aged Brie'
+      Brie
+    when 'Sulfuras, Hand of Ragnaros'
+      Sulfuras
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      Backstage
+    end
   end
 
   def quality
-    return @item.quality if @item
-    @quality
+    @item.quality
   end
 
   def days_remaining
-    return @item.days_remaining if @item
-    @days_remaining
+    @item.days_remaining
   end
 
   def tick
-    case name
-    when 'Normal Item'
-      @item = Normal.new(@quality, @days_remaining)
-      @item.tick
-    when 'Aged Brie'
-      @item = Brie.new(quality, days_remaining)
-      @item.tick
-    when 'Sulfuras, Hand of Ragnaros'
-      @item = Sulfuras.new(@quality, @days_remaining)
-      @item.tick
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      @item = Backstage.new(@quality, @days_remaining)
-      @item.tick
-    end
+    @item.tick
   end
 
 end
